aa3554cad1e08296bca3a82d1528e1b0074c8338
sigi1129 <sigi1129@mju.ac.kr>
2026-03-30 07:27:20 +0000

    정준식 60231944 (1차과제)

--- main.c
+++ main.c
@@ -6,18 +6,12 @@
      하 는 일 : 화면에 hello world를 출력!!!
 */
 
-// 2. 전처리기
-#include <stdio.h>           // 표준 입출력 포함( include)
-#include <stdlib.h>       // system 함수를불러옴
+#include <stdio.h>
 
-// 3. main 함수
-int main(void)
+int main()
 {
-	system("chcp 65001");      // 한글로 나오게
-	
-	printf("hello, world!\n"); // 화면에 hello world 출력
-	printf("정준식\n");
-	printf("첫 번째 줄\n두 번째 줄\n");
-	printf("\"정준식\"\t23\t동탄");
-	return 0;
+  printf("hello world");
+
+  return 0;
+
 }
